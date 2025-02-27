d3e8d7b88046815dafe4e11a2e9d1c13eb5d1dc8
Backbak100 <miko.narayan@gmail.com>
2025-02-26 20:55:20 -0500

    Update drawn_out_input.py
    the lack of commas was annoying me

--- python/drawn_out_input.py
+++ python/drawn_out_input.py
@@ -1,13 +1,13 @@
 printed = False
 
 while not printed:
-    user_input = input("Write hello world: ")
-    if user_input == "hello world":
+    user_input = input("Write 'hello, world': ")
+    if user_input == "hello, world":
         print(user_input)
         break
     else:
         while True:
-            user_input = input("You must write hello world: ")
-            if user_input == "hello world":
+            user_input = input("You must write 'hello, world': ")
+            if user_input == "hello, world":
                 print("Good, now write it like that")
                 break
